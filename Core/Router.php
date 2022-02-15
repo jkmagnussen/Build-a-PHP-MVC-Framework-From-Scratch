ac79386d3b981ec400c4846c864286e7b6d579a5
Joseph Karl Magnussen <jkmagnussen@icloud.com>
2022-02-15 14:07:52 +0000

    router class created

---
+++ Core/Router.php
@@ -0,0 +1,36 @@
+<?php 
+
+/** 
+ * Router 
+ * 
+ * Php version 5.4 + 
+ */
+class Router{
+    
+    /** 
+     * Associative array of routes (the routing table)
+     * @var array
+     */
+    protected $routes = []; // routing table 
+
+    /** 
+     * Add a route to the routing table 
+     * 
+     * @param string $route The route URL 
+     * @param array $params Paramiters (controller, action, etc.)
+     * 
+     * @return void
+     */
+    public function add($route, $params){
+        $this->routes[$route] = $params;
+    }
+
+    /** 
+     * Get all the routes from the routing table 
+     * 
+     * @return array
+     */
+    public function getRoutes(){
+        return $this->routes;
+    }
+}
