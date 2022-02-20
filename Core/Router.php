85e0c2e9988528e84f049070b0192e418c6f3923
Joseph Karl Magnussen <jkmagnussen@icloud.com>
2022-02-20 18:39:46 +0000

    router return

--- Core/Router.php
+++ Core/Router.php
@@ -14,8 +14,13 @@ class Router{
      */
     protected $routes = []; // routing table 
 
+    /** parameters from the matched route 
+     * @var array
+     */
+    protected $params = [];
+
     /** 
-     * Add a route to the routing tablee
+     * Add a route to the routing table
      * 
      * @param string $route The route URL 
      * @param array $params Paramiters (controller, action, etc.)
@@ -34,4 +39,33 @@ class Router{
     public function getRoutes(){
         return $this->routes;
     }
+
+    /** 
+     * Match the route to the routes in the routing table, setting the $params 
+     * property if a route is found.
+     * 
+     * @param string $url The route URL 
+     * 
+     * @return boolean true if a match found, false otherwise 
+     */
+
+     public function match($url){
+         foreach($this->routes as $route => $params){
+             if ($url == $route){
+                 $this->params = $params;
+                 return true;
+             }else{
+                 echo "No route found for URL '$url'";
+             }
+         }
+         return false;
+     }
+
+     /** 
+      * get the currently matched parameters 
+      * @return array
+      */
+      public function getParams(){
+          return $this->params;
+      }
 }
