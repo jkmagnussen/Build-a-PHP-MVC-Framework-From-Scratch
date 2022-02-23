5108ee037b273bc24ca4f8650347a3569a34a19b
Joseph Karl Magnussen <jkmagnussen@icloud.com>
2022-02-23 14:39:47 +0000

    reg ex comp

--- Core/Router.php
+++ Core/Router.php
@@ -35,6 +35,9 @@ class Router{
         // Convert variables e.g. {controller}
         $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z]+)', $route);
 
+        // Convert variables with custom regular expressions e.g. {id: \d}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
         // Add start and end delimiters, and case insensitive flag
         $route = '/^' . $route . '$/i';
 
