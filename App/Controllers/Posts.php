57654e20424c39b7a8935233b0c1f41de4f6377c
Joseph Karl Magnussen <jkmagnussen@icloud.com>
2022-03-18 15:05:47 +0000

    comment minus cap

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -10,7 +10,7 @@ namespace App\Controllers;
 
 class Posts extends Core\Controller{
      /** 
-      * Show the index pageE
+      * Show the index page
       * @return void 
       */
       public function index(){
